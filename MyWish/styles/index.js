81a9d5191cdca45ed61a9a3d70218585a3bdb84f
balatsir <us.balatsir@gmail.com>
2017-06-03 11:50:25 +0300

    03.06.2017 JS Error

---
+++ MyWish/styles/index.js
@@ -0,0 +1,10 @@
+// console.log($('#myModal'));
+// 		$('#myModal').on('shown.bs.modal', function () {
+// 		  $('#myInput').focus()
+// 		})
+ window.onload = function() {
+  };
+  setTimeout(function() {
+	console.log($);
+
+}, 2000);
